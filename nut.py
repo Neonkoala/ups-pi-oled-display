d6e800f9e3bc94585b6aa078abdd0d63d5e8d35c
Nick Dawson <nick@dawson.au>
2023-11-16 22:42:56 +1100

    Nut out a better display

--- nut.py
+++ nut.py
@@ -8,12 +8,15 @@ libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'waveshare')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
+from datetime import timedelta
+from enum import Enum
 import logging
 from nut2 import PyNUTClient
 from waveshare import epd2in13_V3
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
+from uptime import uptime
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -37,26 +40,69 @@ def update_display(full = False):
     upsBatteryRemainingSeconds = ups["battery.runtime"]
     upsBatteryRemaining = str(round(int(upsBatteryRemainingSeconds) / 60))
     upsLoad = ups["ups.load"]
+    upsStatusCode = ups["ups.status"]
 
     # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(libdir, 'Font.ttc'), 15)
-    font24 = ImageFont.truetype(os.path.join(libdir, 'Font.ttc'), 24)
+    font15 = ImageFont.truetype(os.path.join(libdir, 'GillSans.ttf'), 15)
+    font18 = ImageFont.truetype(os.path.join(libdir, 'GillSans.ttf'), 18)
+    font20 = ImageFont.truetype(os.path.join(libdir, 'GillSans.ttf'), 20)
     
     logging.info("1.Drawing on the image...")
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
     draw = ImageDraw.Draw(image)
     
     # Battery icon
-    draw.rectangle([(25,40),(45,50)], outline = 0)
-    draw.rectangle([(15,50),(55,110)], outline = 0)
-    draw.rectangle([(15,50),(55,110)], fill = 0)
+
+    line_thickness = 2
+
+    radius = 5
+    margin = 3
+    spacing = 2
+    height = 106
+    width = 50
+
+    top_inset = 10
+    top_height = 10
+
+    segment_height = 18
+
+    segment_x = margin + line_thickness + spacing
+    segment_y = margin + top_height + line_thickness
+    segment_width = margin + width - line_thickness - spacing
+
+    # Top contact
+    draw.rounded_rectangle([(top_inset + margin, margin), (margin + width - top_inset, margin + top_height)], fill = 0, radius = 5, corners = (True, True, False, False))
+
+    # Casing
+    draw.rounded_rectangle([(margin, margin + top_height),(margin + width, margin + top_height + height)], radius = radius, width = line_thickness, outline = 0)
+
+    segments = 5
+
+    i = 0
+    while i <= segments:
+        rect_index = i + 1
+        draw.rectangle([(segment_x, segment_y + (rect_index * spacing) + (segment_height * i)),(segment_width, segment_y + (rect_index * spacing) + (rect_index * segment_height))], fill = 0)
+        i += 1
+
+    text_margin = 10
+    text_offset = (2 * margin) + width + text_margin
+    text_spacing = 4
+    font = 20
 
     # Info
-    draw.text((0, 0), hostname, font = font24, fill = 0)
-    draw.text((70, 35), upsBatteryCharge + "%", font = font15, fill = 0)
-    draw.text((70, 55), upsBatteryRemaining + " minutes", font = font15, fill = 0)
-    draw.text((70, 75), upsLoad + "W", font = font15, fill = 0)
+    # draw.text((text_offset, margin), hostname, font = font20, fill = 0)
+    # draw.text((text_offset, 35), upsBatteryCharge + "%", font = font18, fill = 0)
+    # draw.text((text_offset, 55), upsBatteryRemaining + " minutes", font = font18, fill = 0)
+    # draw.text((text_offset, 75), upsLoad + "W", font = font18, fill = 0)
 
+    # Status
+    draw.text((text_offset, 0), "Status: " + upsStatusCode, font = font20, fill = 0)
+    draw.text((text_offset, font + text_spacing), upsBatteryCharge + "%", font = font20, fill = 0)
+    draw.text((text_offset, (2 * font) + (2 * text_spacing)), "Remaining: " + upsBatteryRemaining + " mins", font = font20, fill = 0)
+    draw.text((text_offset, (3 * font) + (3 * text_spacing)), "Load: " + upsLoad + "W", font = font20, fill = 0)
+    draw.text((text_offset, (4 * font) + (4 * text_spacing)), "Up: " + str(timedelta(seconds=uptime())).split(".")[0], font = font20, fill = 0)
+
+    # Fix rotation by 180deg
     image = image.rotate(180) # rotate
     if full:
         epd.display(epd.getbuffer(image))
@@ -79,7 +125,7 @@ try:
         logging.info("Display update")
         update_display()
 
-        time.sleep(5.0 - ((time.monotonic() - starttime) % 5.0))
+        time.sleep(1.0 - ((time.monotonic() - starttime) % 1.0))
     
     # # partial update
 #    logging.info("4.show time...")
