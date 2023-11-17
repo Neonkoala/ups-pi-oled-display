4b50810d8915f1fd1bf0cd37c66a44012458daf5
Nick Dawson <nick@dawson.au>
2023-11-17 22:29:21 +1100

    Updated battery to reflect charge, interpret Cyberpower statuses for UIw

--- nut.py
+++ nut.py
@@ -23,6 +23,59 @@ logging.basicConfig(level=logging.DEBUG)
 starttime = time.monotonic()
 epd = epd2in13_V3.EPD()
 
+class UPSStatus(Enum):
+    online = "OL"
+    online_low_battery = "OL LB"
+    online_low_voltage = "OL BOOST"
+    online_charge = "OL CHRG"
+    online_charge_low_battery = "OL CHRG LB"
+    online_discharge = "OL DISCHRG"
+    online_discharge_low_battery = "OL DISCHRG LB"
+    battery_discharge = "OB DISCHRG"
+    battery_discharge_low_battery = "OB DISCHRG LB"
+    battery_low_battery = "OB LB"
+    replace_battery = "RB"
+    bypass = "BYPASS"
+
+class UPSOnlineStatus(Enum):
+    online = 1
+    offline = 2
+    unknown = 3
+
+class UPSChargeStatus(Enum):
+    charging = 1
+    discharging = 2
+    none = 3
+
+class UPSOtherStatus(Enum):
+    low_battery = 1
+    low_voltage = 2
+    none = 3
+
+def ups_online_status(status: UPSStatus):
+    if status in [UPSStatus.online, UPSStatus.online_low_battery, UPSStatus.online_low_voltage, UPSStatus.online_charge, UPSStatus.online_charge_low_battery, UPSStatus.online_discharge, UPSStatus.online_discharge_low_battery]:
+        return UPSOnlineStatus.online
+    elif status in [UPSStatus.battery_discharge, UPSStatus.battery_discharge_low_battery, UPSStatus.battery_low_battery]:
+        return UPSOnlineStatus.offline
+    else:
+        return UPSOnlineStatus.unknown
+
+def ups_charge_status(status: UPSStatus):
+    if status in [UPSStatus.online_charge, UPSStatus.online_charge_low_battery]:
+        return UPSChargeStatus.charging
+    elif status in [UPSStatus.battery_discharge, UPSStatus.battery_discharge_low_battery, UPSStatus.online_discharge, UPSStatus.online_discharge_low_battery]:
+        return UPSChargeStatus.discharging
+    else:
+        return UPSChargeStatus.none
+
+def ups_other_status(status: UPSStatus):
+    if status == UPSStatus.online_low_voltage:
+        return UPSOtherStatus.low_voltage
+    elif status in [UPSStatus.online_low_battery, UPSStatus.online_charge_low_battery, UPSStatus.online_discharge_low_battery, UPSStatus.battery_discharge_low_battery, UPSStatus.battery_low_battery]:
+        return UPSOtherStatus.low_battery
+    else:
+        return UPSOtherStatus.none
+
 def update_display(full = False):
     # Init
     hostname = "localhost"
@@ -64,11 +117,13 @@ def update_display(full = False):
     top_inset = 10
     top_height = 10
 
+    segment_count = 5
     segment_height = 18
 
     segment_x = margin + line_thickness + spacing
     segment_y = margin + top_height + line_thickness
     segment_width = margin + width - line_thickness - spacing
+    segment_max = segment_y + (segment_height * segment_count) + (spacing * segment_count)
 
     # Top contact
     draw.rounded_rectangle([(top_inset + margin, margin), (margin + width - top_inset, margin + top_height)], fill = 0, radius = 5, corners = (True, True, False, False))
@@ -76,12 +131,13 @@ def update_display(full = False):
     # Casing
     draw.rounded_rectangle([(margin, margin + top_height),(margin + width, margin + top_height + height)], radius = radius, width = line_thickness, outline = 0)
 
-    segments = 5
+    # Calculate & draw battery segments
+    draw_segments = (float(upsBatteryCharge)/100) * segment_count
 
     i = 0
-    while i <= segments:
+    while i < draw_segments - 1:
         rect_index = i + 1
-        draw.rectangle([(segment_x, segment_y + (rect_index * spacing) + (segment_height * i)),(segment_width, segment_y + (rect_index * spacing) + (rect_index * segment_height))], fill = 0)
+        draw.rectangle([(segment_x, segment_max - (i * spacing) - (rect_index * segment_height)), (segment_width, segment_max - (i * spacing) - (i * segment_height))], fill = 0)
         i += 1
 
     text_margin = 10
@@ -89,15 +145,32 @@ def update_display(full = False):
     text_spacing = 4
     font = 20
 
-    # Info
-    # draw.text((text_offset, margin), hostname, font = font20, fill = 0)
-    # draw.text((text_offset, 35), upsBatteryCharge + "%", font = font18, fill = 0)
-    # draw.text((text_offset, 55), upsBatteryRemaining + " minutes", font = font18, fill = 0)
-    # draw.text((text_offset, 75), upsLoad + "W", font = font18, fill = 0)
-
     # Status
-    draw.text((text_offset, 0), "Status: " + upsStatusCode, font = font20, fill = 0)
-    draw.text((text_offset, font + text_spacing), upsBatteryCharge + "%", font = font20, fill = 0)
+    status = UPSStatus(upsStatusCode)
+    online_status = ups_online_status(status)
+    charge_status = ups_charge_status(status)
+    other_status = ups_other_status(status)
+
+    status_text = "Unknown"
+    if online_status == UPSOnlineStatus.online:
+        status_text = "Online"
+    elif online_status == UPSOnlineStatus.offline:
+        status_text = "Offline"
+
+    if other_status == UPSOtherStatus.low_voltage:
+        status_text += " (Low Voltage)"
+    elif other_status == UPSOtherStatus.low_battery:
+        status_text += " (Low Battery)"
+
+    battery_status_text = "%"
+    if charge_status == UPSChargeStatus.charging:
+        battery_status_text = "% Charging"
+    elif charge_status == UPSChargeStatus.discharging:
+        battery_status_text = "% Discharging"
+
+    # Draw text
+    draw.text((text_offset, 0), status_text, font = font20, fill = 0)
+    draw.text((text_offset, font + text_spacing), upsBatteryCharge + battery_status_text, font = font20, fill = 0)
     draw.text((text_offset, (2 * font) + (2 * text_spacing)), "Remaining: " + upsBatteryRemaining + " mins", font = font20, fill = 0)
     draw.text((text_offset, (3 * font) + (3 * text_spacing)), "Load: " + upsLoad + "W", font = font20, fill = 0)
     draw.text((text_offset, (4 * font) + (4 * text_spacing)), "Up: " + str(timedelta(seconds=uptime())).split(".")[0], font = font20, fill = 0)
